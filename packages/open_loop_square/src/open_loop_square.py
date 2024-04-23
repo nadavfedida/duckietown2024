c49a8c8eff637e8832ced41b4eb353e3b4a85492
nadavfedida <nadavz_@hotmail.com>
2024-04-23 12:45:23 +1000

    modified code for encoder use

--- packages/open_loop_square/src/open_loop_square.py
+++ packages/open_loop_square/src/open_loop_square.py
@@ -2,65 +2,92 @@
 
 import rospy
 from duckietown_msgs.msg import Twist2DStamped
-from duckietown_msgs.msg import FSMState
- 
-class Drive_Square:
+from duckietown_msgs.msg import FSMState, Range
+from duckietown_msgs.msg import WheelIsCmdStamped, EncoderStamped
+
+class DuckiebotMovement:
+    self.distance = 0
+    self.Min_range = 0.5
+
     def __init__(self):
-        #Initialize global class variables
+        #self._left_encoder_topic
+        rospy.init_node('duckiebot_movement_custom', anonymous=True)
+        self.pub = rospy.Publisher('/duckienadav/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=10)
+        rospy.Subscriber('/duckienadav/fsm_node/mode', FSMState, self.fsm_callback)
+
+        # subscribe to encoder topic - left wheel 
+        rospy.Subscriber('/duckienadav/left_wheel_encoder_node/tick', EncoderStamped, encoder_callback)
+        rospy.wait_for_message('/duckienadav/left_wheel_encoder_node/tick', EncoderStamped)
+
+
+        # Subscribe to ToF distance finder
+        #rospy.Subscriber('/duckienadav/front_center_tof_driver_node/range', Range, self.distance_check)
         self.cmd_msg = Twist2DStamped()
 
-        #Initialize ROS node
-        rospy.init_node('drive_square_node', anonymous=True)
-        
-        #Initialize Pub/Subs
-        self.pub = rospy.Publisher('/duckienadav/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=1)
-        rospy.Subscriber('/duckienadav/fsm_node/mode', FSMState, self.fsm_callback, queue_size=1)
-        
-    # robot only moves when lane following is selected on the duckiebot joystick app
+    #def distance_check(self, msg):
+        #self.distance = msg.range    
+        #add function to check distance and stop if less than X
+
+    # Function to save wheel encoder values to global variables
+    def encoder_callback(msg):
+        global left_encoder_ticks
+        global right_encoder_ticks
+        left_encoder_ticks = msg.left_ticks
+        right_encoder_ticks = msg.right_ticks
+
+
+
     def fsm_callback(self, msg):
-        rospy.loginfo("State: %s", msg.state)
         if msg.state == "NORMAL_JOYSTICK_CONTROL":
             self.stop_robot()
-        elif msg.state == "LANE_FOLLOWING":            
-            rospy.sleep(1) # Wait for a sec for the node to be ready
+        elif msg.state == "LANE_FOLLOWING":
             self.move_robot()
- 
-    # Sends zero velocities to stop the robot
+
     def stop_robot(self):
         self.cmd_msg.header.stamp = rospy.Time.now()
         self.cmd_msg.v = 0.0
         self.cmd_msg.omega = 0.0
         self.pub.publish(self.cmd_msg)
- 
-    # Spin forever but listen to message callbacks
-    def run(self):
-    	rospy.spin() # keeps node from exiting until node has shutdown
 
-    # Robot drives in a square and then stops
     def move_robot(self):
+        #   Set initial tick values
+        left_encoder_ticks = 0
+        right_encoder_ticks = 0
 
-        #YOUR CODE GOES HERE#
-        self.cmd_msg.header.stamp = rospy.Time.now()
-        self.cmd_msg.v = 0.5 # striaght line velocity
-        self.cmd_msg.omega = 0.0
-        self.pub.publish(self.cmd_msg)
-        rospy.loginfo("Forward!")
-        rospy.sleep(1) # straight line driving time
-        
-        self.cmd_msg.header.stamp = rospy.Time.now()
-        self.cmd_msg.v = -0.5 # striaght line velocity
-        self.cmd_msg.omega = 0.0
-        self.pub.publish(self.cmd_msg)
-        rospy.loginfo("Backward!")
-        rospy.sleep(1) # straight line driving time
-        
-        ######################
-                
+        desired_ticks = 1000
+        speed = 0.3
+        while (left_encoder_ticks < desired_ticks):    
+            self.cmd_msg.header.stamp = rospy.Time.now()
+            self.cmd_msg.v = speed
+            self.cmd_msg.omega = 0.0
+            self.pub.publish(self.cmd_msg)
         self.stop_robot()
+
+
+
+
+        # for i in range(4):
+        #     while (self.distance > self.Min_range):    
+        #         self.cmd_msg.header.stamp = rospy.Time.now()
+        #         self.cmd_msg.v = 0.41
+        #         self.cmd_msg.omega = 0.0
+        #         self.pub.publish(self.cmd_msg)
+        #         rospy.sleep(2)
+        #         self.cmd_msg.header.stamp = rospy.Time.now()
+        #         self.cmd_msg.v = 0.0
+        #         self.cmd_msg.omega = -8.3
+        #         self.pub.publish(self.cmd_msg)
+        #         rospy.sleep(0.2)
+        #     self.stop_robot()
+        # self.stop_robot()
+
+    def run(self):
+        rospy.spin()
 
 if __name__ == '__main__':
     try:
-        duckiebot_movement = Drive_Square()
+        duckiebot_movement = DuckiebotMovement()
         duckiebot_movement.run()
     except rospy.ROSInterruptException:
         pass
+
